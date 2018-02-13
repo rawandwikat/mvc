286c699a140b5f75607503f49dbb4514777a4f64
jccn <jccn@jcc>
2018-02-13 05:21:40 +0200

    dfghjk

---
+++ src/calculatorhm/A.java
@@ -0,0 +1,20 @@
+package calculatorhm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class A {
+
+	@Test
+	public void test() {
+		CalculatorhmModal c=new CalculatorhmModal();
+		c.div(8, 0);
+		assertEquals(-0,-0);
+		c.squer(-1);
+		assertEquals(-0,-0);
+		c.mod(7, 0);
+		assertEquals(-0,-0);
+	}
+
+}
